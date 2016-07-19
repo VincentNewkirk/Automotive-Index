54a266d8f44d5a7f6c58badfaf01e85483bbd792
Vincent <vincentnewkirk@gmail.com>
2016-07-19 13:56:24 -1000

    completed all tasks

--- indexing.sql
+++ indexing.sql
@@ -39,15 +39,50 @@ SELECT make_code, model_code, model_title, year
 FROM car_models
 WHERE make_code = 'LAM';
 
+
 SELECT *
 FROM car_models
 WHERE year BETWEEN 2010 AND 2015;
+--78.960ms
 
 SELECT *
 FROM car_models
 WHERE year = 2010;
 --33.921ms
 
+SELECT DISTINCT make_title
+FROM car_models
+WHERE make_code = 'LAM';
+--24.455 ms
 
+SELECT DISTINCT model_title
+FROM car_models
+WHERE model_code = 'GT-R'
+AND make_code = 'NISSAN';
+--0.367ms
+
+SELECT make_code, model_code, model_title, year
+FROM car_models
+WHERE make_code = 'LAM';
+--24.516ms
+
+SELECT *
+FROM car_models
+WHERE year = 2010;
+--14.457ms
+
+DROP TABLE car_models;
+\i scripts/car_models.sql;
+
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
 
 \timing;
