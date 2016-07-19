7f16a4cbaadf5b14178e6b7d6d0eac8241152f3a
Vincent <vincentnewkirk@gmail.com>
2016-07-19 10:22:54 -1000

    timing select completed

---
+++ indexing.sql
@@ -0,0 +1,53 @@
+\timing
+--1
+--CREATE USER indexed_cars_user;
+
+--2
+DROP DATABASE IF EXISTS indexed_cars;
+
+CREATE DATABASE indexed_cars OWNER indexed_cars_user;
+
+\c indexed_cars;
+--3
+\i scripts/car_models.sql;
+
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
+--SQL is weak
+
+
+SELECT DISTINCT make_title
+FROM car_models
+WHERE make_code = 'LAM';
+--135.306ms
+
+SELECT DISTINCT model_title
+FROM car_models
+WHERE model_code = 'GT-R'
+AND make_code = 'NISSAN';
+--252.217ms
+
+SELECT make_code, model_code, model_title, year
+FROM car_models
+WHERE make_code = 'LAM';
+
+SELECT *
+FROM car_models
+WHERE year BETWEEN 2010 AND 2015;
+
+SELECT *
+FROM car_models
+WHERE year = 2010;
+--33.921ms
+
+
+
+\timing;
